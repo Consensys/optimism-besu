642d150abc5a5358f5bff2a832d9d4fc2401a213
thinkAfCod <q315xia@163.com>
2025-02-03 11:08:27 +0800

    separate the optimism transaction type list from the mainnet transaction list
    Signed-off-by: thinkAfCod <q315xia@163.com>

---
+++ ethereum/core/src/main/java/org/hyperledger/besu/ethereum/core/encoding/MainnetTransactionEncoderDecoderProvider.java
@@ -0,0 +1,84 @@
+/*
+ * Copyright ConsenSys AG.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+package org.hyperledger.besu.ethereum.core.encoding;
+
+import com.google.common.collect.ImmutableMap;
+import org.hyperledger.besu.datatypes.TransactionType;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+/**
+ * The transaction encoder provider for Mainnet.
+ */
+public class MainnetTransactionEncoderDecoderProvider implements TransactionEncoder.EncoderProvider,TransactionDecoder.DecoderProvider  {
+
+  private static final ImmutableMap<TransactionType, TransactionEncoder.Encoder> TYPED_TRANSACTION_ENCODERS =
+      ImmutableMap.of(
+          TransactionType.ACCESS_LIST,
+          AccessListTransactionEncoder::encode,
+          TransactionType.EIP1559,
+          EIP1559TransactionEncoder::encode,
+          TransactionType.BLOB,
+          BlobTransactionEncoder::encode,
+          TransactionType.DELEGATE_CODE,
+          CodeDelegationTransactionEncoder::encode);
+
+  private static final ImmutableMap<TransactionType, TransactionDecoder.Decoder> TYPED_TRANSACTION_DECODERS =
+      ImmutableMap.of(
+          TransactionType.ACCESS_LIST,
+          AccessListTransactionDecoder::decode,
+          TransactionType.EIP1559,
+          EIP1559TransactionDecoder::decode,
+          TransactionType.BLOB,
+          BlobTransactionDecoder::decode,
+          TransactionType.DELEGATE_CODE,
+          CodeDelegationTransactionDecoder::decode);
+
+  private static final ImmutableMap<TransactionType, TransactionEncoder.Encoder> POOLED_TRANSACTION_ENCODERS =
+      ImmutableMap.of(TransactionType.BLOB, BlobPooledTransactionEncoder::encode);
+
+  private static final ImmutableMap<TransactionType, TransactionDecoder.Decoder> POOLED_TRANSACTION_DECODERS =
+      ImmutableMap.of(TransactionType.BLOB, BlobPooledTransactionDecoder::decode);
+
+  @Override
+  public TransactionEncoder.Encoder getEncoder(
+      final TransactionType transactionType, final EncodingContext encodingContext) {
+
+    if (encodingContext.equals(EncodingContext.POOLED_TRANSACTION)) {
+      if (POOLED_TRANSACTION_ENCODERS.containsKey(transactionType)) {
+        return POOLED_TRANSACTION_ENCODERS.get(transactionType);
+      }
+    }
+    return checkNotNull(
+        TYPED_TRANSACTION_ENCODERS.get(transactionType),
+        "Developer Error. A supported transaction type %s has no associated encoding logic",
+        transactionType);
+  }
+
+  @Override
+  public TransactionDecoder.Decoder getDecoder(
+      final TransactionType transactionType, final EncodingContext encodingContext) {
+    if (encodingContext.equals(EncodingContext.POOLED_TRANSACTION)) {
+      if (POOLED_TRANSACTION_DECODERS.containsKey(transactionType)) {
+        return POOLED_TRANSACTION_DECODERS.get(transactionType);
+      }
+    }
+    return checkNotNull(
+        TYPED_TRANSACTION_DECODERS.get(transactionType),
+        "Developer Error. A supported transaction type %s has no associated decoding logic",
+        transactionType);
+  }
+
+}
--- ethereum/core/src/main/java/org/hyperledger/besu/ethereum/core/encoding/TransactionEncoder.java
+++ ethereum/core/src/main/java/org/hyperledger/besu/ethereum/core/encoding/TransactionEncoder.java
@@ -28,25 +28,32 @@ import org.apache.tuweni.bytes.Bytes;
 public class TransactionEncoder {
 
   @FunctionalInterface
-  interface Encoder {
+  public interface Encoder {
     void encode(Transaction transaction, RLPOutput output);
   }
 
-  private static final ImmutableMap<TransactionType, Encoder> TYPED_TRANSACTION_ENCODERS =
-      ImmutableMap.of(
-          TransactionType.ACCESS_LIST,
-          AccessListTransactionEncoder::encode,
-          TransactionType.EIP1559,
-          EIP1559TransactionEncoder::encode,
-          TransactionType.BLOB,
-          BlobTransactionEncoder::encode,
-          TransactionType.DELEGATE_CODE,
-          CodeDelegationTransactionEncoder::encode,
-          TransactionType.OPTIMISM_DEPOSIT,
-          OptimismDepositTransactionEncoder::encode);
+  @FunctionalInterface
+  public interface EncoderProvider {
+    /**
+     * Gets the encoder for a given transaction type and encoding context. If the context is
+     * POOLED_TRANSACTION, it uses the network encoder for the type. Otherwise, it uses the typed
+     * encoder.
+     *
+     * @param transactionType the transaction type
+     * @param encodingContext the encoding context
+     * @return the encoder
+     */
+    Encoder getEncoder(TransactionType transactionType, EncodingContext encodingContext);
+  }
 
-  private static final ImmutableMap<TransactionType, Encoder> POOLED_TRANSACTION_ENCODERS =
-      ImmutableMap.of(TransactionType.BLOB, BlobPooledTransactionEncoder::encode);
+  /**
+   * The Encoder provider.
+   */
+  private static EncoderProvider encoderProvider = new MainnetTransactionEncoderDecoderProvider();
+
+  public static void setEncoderProvider(final EncoderProvider encoderProvider) {
+    TransactionEncoder.encoderProvider = encoderProvider;
+  }
 
   /**
    * Encodes a transaction into RLP format.
@@ -122,17 +129,8 @@ public class TransactionEncoder {
         transaction.getType(), "Transaction type for %s was not specified.", transaction);
   }
 
-  private static Encoder getEncoder(
+  private static TransactionEncoder.Encoder getEncoder(
       final TransactionType transactionType, final EncodingContext encodingContext) {
-
-    if (encodingContext.equals(EncodingContext.POOLED_TRANSACTION)) {
-      if (POOLED_TRANSACTION_ENCODERS.containsKey(transactionType)) {
-        return POOLED_TRANSACTION_ENCODERS.get(transactionType);
-      }
-    }
-    return checkNotNull(
-        TYPED_TRANSACTION_ENCODERS.get(transactionType),
-        "Developer Error. A supported transaction type %s has no associated encoding logic",
-        transactionType);
+    return TransactionEncoder.encoderProvider.getEncoder(transactionType, encodingContext);
   }
 }
